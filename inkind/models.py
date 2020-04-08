c6aceae8955c57417144c76ac700a5ae3ee8f53a
Karrie Dvorak <dvorak.karrie@gmail.com>
2020-04-08 13:38:38 -0500

    adding first and last name volunteer fields and sorting on both tables

--- inkind/models.py
+++ inkind/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Volunteer(models.Model):
-    name = models.CharField(max_length=100, null=True)
+    first_name = models.CharField(max_length=100, null=True)
+    last_name = models.CharField(max_length=100, null=True)
     email_address = models.CharField(default='@gmail.com', max_length=100)
-
+    
     def __str__(self):
-        return self.name
+        return f"{self.first_name} {self.last_name}"
 
 class Service(models.Model):
     volunteer = models.ForeignKey(Volunteer, on_delete=models.CASCADE, related_name='services')
@@ -16,4 +17,5 @@ class Service(models.Model):
     hourly_rate = models.DecimalField(max_digits=5, decimal_places=2)
     total_value_of_service = models.DecimalField(max_digits=7, decimal_places=2)
 
-    
+    def __str__(self):
+        return f"{self.volunteer} - {self.service_dates}"
--- inkind/views.py
+++ inkind/views.py
@@ -3,10 +3,9 @@ from .models import Volunteer, Service
 
 # Create your views here.
 def volunteer_list(request):
-    volunteers = Volunteer.objects.all()
+    volunteers = Volunteer.objects.order_by('last_name', 'first_name')
     return render(request, 'inkind/volunteer_list.html', {'volunteers': volunteers})
 
 def service_list(request):
-    services = Service.objects.all()
+    services = Service.objects.order_by('volunteer', 'service_dates')
     return render(request, 'inkind/service_list.html', {'services': services})
-    
