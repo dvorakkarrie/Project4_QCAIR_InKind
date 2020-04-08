4846943c661c9c33ccafb72056a997c5571d0ca3
Karrie Dvorak <dvorak.karrie@gmail.com>
2020-04-08 13:05:45 -0500

    set up volunteer_list and service_list templates

--- inkind/urls.py
+++ inkind/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.volunteer_list, name='volunteer_list'),
-    path('',views.service_list, name='service_list'),
+    path('service/',views.service_list, name='service_list'),
 ]
