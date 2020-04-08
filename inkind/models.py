ac2a73b34fe20aa9713c71fea702c90c7a8d1b11
Karrie Dvorak <dvorak.karrie@gmail.com>
2020-04-08 11:25:01 -0500

    set up service database

--- inkind/models.py
+++ inkind/models.py
@@ -2,8 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Volunteer(models.Model):
-    name = models.CharField(max_length=100)
-    email_address = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, null=True)
+    email_address = models.CharField(default='@gmail.com', max_length=100)
 
     def __str__(self):
         return self.name
+
+class Service(models.Model):
+    volunteer = models.ForeignKey(Volunteer, on_delete=models.CASCADE, related_name='services')
+    dates = models.CharField(max_length=100, null=True)
+    hours = models.IntegerField(null=True)
+    description = models.TextField(default='provide description of services performed')
+    rate = models.DecimalField(max_digits=3, decimal_places=2)
+    total = models.DecimalField(max_digits=5, decimal_places=2)
+
+    
