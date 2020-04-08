67cd1b67241f86a0deec1d2b35bb16010ec63c3b
Karrie Dvorak <dvorak.karrie@gmail.com>
2020-04-08 14:57:05 -0500

    set up service and volunteer detail pages

--- inkind/views.py
+++ inkind/views.py
@@ -6,6 +6,14 @@ def volunteer_list(request):
     volunteers = Volunteer.objects.order_by('last_name', 'first_name')
     return render(request, 'inkind/volunteer_list.html', {'volunteers': volunteers})
 
+def volunteer_detail(request, pk):
+    volunteer = Volunteer.objects.get(id=pk)
+    return render(request, 'inkind/volunteer_detail.html', {'volunteer': volunteer})
+
 def service_list(request):
     services = Service.objects.order_by('volunteer', 'year', 'month')
     return render(request, 'inkind/service_list.html', {'services': services})
+
+def service_detail(request, pk):
+    service = Service.objects.get(id=pk)
+    return render(request, 'inkind/service_detail.html', {'service': service})
