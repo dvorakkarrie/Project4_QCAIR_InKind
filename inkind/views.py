a8d436d3c58029690def24b82427535635e3c910
Karrie Dvorak <dvorak.karrie@gmail.com>
2020-04-08 12:39:22 -0500

    set up volunteer and service views

--- inkind/views.py
+++ inkind/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Volunteer, Service
 
 # Create your views here.
+def volunteer_list(request):
+    volunteers = Volunteer.objects.all()
+    return render(request, 'inkind/volunteer_list.html', {'volunteers': volunteers})
+
+def service_list(request):
+    services = Service.objects.all()
+    return render(request, 'inkind/service_list.html', {'services': services})
+    
