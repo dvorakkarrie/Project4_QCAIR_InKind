118eb0cd0688170818fbb0ad666e6fc306572e32
Karrie Dvorak <dvorak.karrie@gmail.com>
2020-04-08 22:30:10 -0500

    changing header style on list htmls and css file

--- inkind_project/urls.py
+++ inkind_project/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('inkind.urls')),
     path('', include('accounts.urls')),
+    url('^oauth/', include('social_django.urls', namespace='social')), 
 ]
