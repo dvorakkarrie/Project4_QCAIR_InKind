4ea6412a480e25cca3ef8d7d1cab774213a5ed6d
Karrie Dvorak <dvorak.karrie@gmail.com>
2020-04-10 12:34:45 -0500

    resolved issue with backend

--- inkind_project/settings.py
+++ inkind_project/settings.py
@@ -123,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.github.GithubOAuth2',
-    'social_core.backends.twitter.TwitterOAuth',
+    # 'social_core.backends.twitter.TwitterOAuth',
     'social_core.backends.facebook.FacebookOAuth2',
 
     'django.contrib.auth.backends.ModelBackend',
